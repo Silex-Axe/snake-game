52f841758d8691f63e449f87c04bdaa8b45153f1
Alex <alex.melcon7@gmail.com>
2019-04-23 11:11:54 -0400

    Fixed

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
 # TODO list
-Add score system
-Improve the user experience
-Add bonuses (speed, duble score for a period?)
 Memory optimization: Save only the turning points as long as they are still there
+Improve the user experience
+Add game over sound
+Refactor
+Add bonuses (speed, duble score for a period?)
+Add bonus sound
 Restrict appearece of the food to non occupied spots
--- snake.js
+++ snake.js
@@ -2,6 +2,7 @@
 const FRAME_RATE = 20
 //Scale of the game, the bigger it is, the 'pixels' get bigger
 const ITEMS_SCALE = 10;
+const BASIC_FOOD_SCORE = 10;
 
 const COIN_SOUND_PATH = 'assets/coin.mp3'
 const GAME_FONT_PATH = 'assets/game_over.ttf'
@@ -29,7 +30,7 @@ function setup() {
 
 /*Draw every tick of the clock (FRAME_RATE)*/
 function draw() {
-    background(51);
+    background(10,0,40);
     environment.draw()
 }
 
@@ -84,16 +85,20 @@ function Game(cols,rows){
     started=false
     /* Initialize the snake and the food location */
     this.init = function(){
+        this.score=0
         this.snake = new Snake()
         this.pickFoodLocation();
     }
+    /* Start the game */
     this.start = function(){
         this.started = true;
     }
+    /* Pick a random position and put a coin there */
     this.pickFoodLocation = function() {
         this.food = createVector(floor(random(cols)), floor(random(rows)))
         this.food.mult(ITEMS_SCALE)
     }
+    
     this.manageInput=function(keyCode){
         if (keyCode === UP_ARROW) {
             this.snake.dir(0, -1);
@@ -105,47 +110,51 @@ function Game(cols,rows){
             this.snake.dir(-1, 0);
         }
     }
+    
 /* Draw the components of the game */
     this.draw = function(){
-        if(this.started){
-         this.snake.update();
-        }
-        this.snake.show();
         if (this.snake.eat(this.food)) {
             this.pickFoodLocation();
             if (coin_sound.isPlaying()) {
                 coin_sound.stop();
-              } 
+            }
             coin_sound.play();
-              
+            this.score += BASIC_FOOD_SCORE
+        }
+        if(this.started){
+         this.snake.update();
         }
         if(this.snake.checkSelfCollision()){
             this.started = false;
         }
-
+    
+        textFont(game_font);
+        textSize(height / 6);
+        textAlign(CENTER, CENTER);
+        fill(20,200,200);
+        text(this.score+'', width*0.8, 0, width*0.2, height/6);
+        this.snake.show();
         fill(255, 200, 0);
         rect(this.food.x, this.food.y, ITEMS_SCALE, ITEMS_SCALE);
-
     }
 }
+
 /** 
  * Snake object, holds position of the snake, speed and every pixel of it 
  * */
 function Snake() {
     this.x = 0;
     this.y = 0;
-
     this.xspeed = 1;
     this.yspeed = 0;
-
-    this.total = 1;
     this.tail = [];
 
     this.dir = function (xdir, ydir) {  
-        if(this.xspeed!=-xdir)  {
-            this.xspeed = xdir;
+        if(this.xspeed!=-xdir)  {            
+        this.xspeed = xdir;
         }
         if(this.yspeed != -ydir){
+            //Can turn
             this.yspeed = ydir;
         }
     }
@@ -153,30 +162,27 @@ function Snake() {
     this.eat = function (pos) {
         var d = dist(this.x, this.y, pos.x, pos.y);
         if (d < 1) {
-            this.total++;
+            console.log('I am fed')
+            this.tail.push(this.tail[this.tail.length-1])
             return true;
         } else {
             return false;
         }
     }
-    this.checkSelfCollision = function(){
-        for (var i = 1; i < this.tail.length ; i++){
-         if(this.tail[0].x==this.tail[i].x && this.tail[0].y==this.tail[i].y ) {
-            console.log("I ate myself :(")
-            return true;
-         }
-         return false;
-        }
+
+    this.checkSelfCollision = function(){    
+        let same = this.tail.slice(1).find((v)=>this.tail[0].x==v.x && this.tail[0].y==v.y)
+        return same!=undefined;
     }
+
     this.update = function () {
         //Move every point forward
-        if (this.total === this.tail.length) {
-            for (var i = 0; i < this.tail.length - 1; i++) {
-                this.tail[i] = this.tail[i + 1]
+        for (var i = this.tail.length-1; i > 0; i--) {
+            this.tail[i] = this.tail[i - 1];
             }
-        }
+        this.tail[0] = createVector(this.x, this.y)
+
         // New head point
-        this.tail[this.total - 1] = createVector(this.x, this.y)
         this.x = this.x + this.xspeed * ITEMS_SCALE;
         this.y = this.y + this.yspeed * ITEMS_SCALE;
         this.x = constrain(this.x, 0, width - ITEMS_SCALE)
